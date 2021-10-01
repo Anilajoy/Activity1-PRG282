5171a0be1a2aa07c6d0dfcbfa160ea5dbd89508e
Anilajoy <elzaanila2004@gmail.com>
2021-10-01 09:48:14 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz_PRG282_Git
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
